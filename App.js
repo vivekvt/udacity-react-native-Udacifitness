963b8a0e2191998124f2e2de2b5826dfc60b33af
VTVIVEK <vivek78643@gmail.com>
2020-05-26 16:08:52 +0530

    added tab navigation

--- App.js
+++ App.js
@@ -1,19 +1,105 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import AddEntry from './components/AddEntry';
-import History from './components/History';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import React, {Component} from 'react';
+import 'react-native-gesture-handler';
+import {Dimensions, Platform, StatusBar, View, Text} from 'react-native';
+import {createStore} from "redux";
 import reducer from './reducers';
+import {Provider} from "react-redux";
+import History from "./components/History";
+import AddEntry from "./components/AddEntry";
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {purple, white} from "./utils/colors";
+import {FontAwesome, Ionicons} from "@expo/vector-icons";
+import Constants from "expo-constants";
+import {createStackNavigator} from '@react-navigation/stack';
+// import EntryDetail from "./components/EntryDetail";
+// import Live from "./components/Live";
 
-export default function App() {
-  return (
-    <Provider store={createStore(reducer)}>
-    <View style={{flex:1}}>
-      <View style={{height:20}}/>
-      <History />
-      {/* <AddEntry/> */}
-    </View>
-    </Provider>
-  );
+
+function UdaciStatusBar({backgroundColor, ...props}) {
+    return (
+        <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+        </View>
+    )
+}
+
+function Live(){
+  return(<Text>Live</Text>)
+}
+function EntryDetail(){
+    return(<Text>EntryDetail</Text>)
+}
+
+
+const Tabs =
+    Platform.OS === "ios"
+        ? createBottomTabNavigator()
+        : createMaterialTopTabNavigator();
+
+
+const TabNav = () => (
+    <Tabs.Navigator
+        initialRouteName="AddEntry"
+        screenOptions={({route}) => ({
+            tabBarIcon: ({color, size}) => {
+                switch (route.name) {
+                    case "Add Entry": {
+                        return <FontAwesome name="plus-square" size={size} color={color}/>
+                    }
+                    case "History": {
+                        return <Ionicons name="ios-bookmarks" size={size} color={color}/>
+                    }
+                    case "Live": {
+                        return <Ionicons name="ios-speedometer" size={size} color={color}/>
+                    }
+                    default:
+                        return ""
+                }
+            }
+        })}
+        tabBarOptions={{
+            header: null,
+            activeTintColor: Platform.OS === "ios" ? purple : white,
+            showIcon: Platform.OS === "ios",
+            style: {
+                height: Platform.OS === "ios" ? 80 : 50,
+                backgroundColor: Platform.OS === "ios" ? white : purple,
+                shadowColor: "rgba(0, 0, 0, 0.24)",
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
+        }}
+    >
+        <Tabs.Screen name="Add Entry" component={AddEntry}/>
+        <Tabs.Screen name="History" component={History}/>
+        <Tabs.Screen name="Live" component={Live}/>
+    </Tabs.Navigator>
+);
+
+
+// 
+
+
+export default class App extends Component {
+
+
+    render() {
+        return (
+            <Provider store={createStore(reducer)}>
+                <View style={{flex: 1}}>
+                    <NavigationContainer>
+                        <UdaciStatusBar backgroundColor={purple} barStyle="light-content"/>
+                        <MyStack/>
+                    </NavigationContainer>
+                </View>
+            </Provider>
+
+        );
+    }
 }
